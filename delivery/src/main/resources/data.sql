497b62c7015de8db48f2fb47291cdd1006d22365
Kulonbekov <kulonbekov@gmail.com>
2023-05-23 16:12:57 +0600

    add entries in file data.sql

--- delivery/src/main/resources/data.sql
+++ delivery/src/main/resources/data.sql
@@ -1,3 +1,8 @@
 insert into tb_order (sender_name, sender_city, sender_address,recipient_name, recipient_city,recipient_address, package_weight,sending_date,status)
-VALUES ('Kurmanaliev T.K.', 'Karakol', 'Tynystanova 123', 'Primberdiev A.D.', 'Bishkek', 'Chuy prospect 33', 3.4, '23.05.2023' , 'ACTIVE');
+VALUES ('Kurmanaliev T.K.', 'Karakol', 'Tynystanova 123', 'Primberdiev A.D.', 'Bishkek', 'Ahunbaeva 33', 3.4, '23.05.2023' , 'ACTIVE');
 
+insert into tb_order (sender_name, sender_city, sender_address,recipient_name, recipient_city,recipient_address, package_weight,sending_date,status)
+VALUES ('Kurmanaliev T.K.', 'Karakol', 'Tynystanova 123', 'Alihanov B.D.', 'Balykchy', 'Smirnova 12', 2.1, '22.05.2023' , 'ACTIVE');
+
+insert into tb_order (sender_name, sender_city, sender_address,recipient_name, recipient_city,recipient_address, package_weight,sending_date,status)
+VALUES ('Kurmanaliev T.K.', 'Karakol', 'Tynystanova 123', 'Adilhanov T.K.', 'Bishkek', 'Mederova 54', 4.5, '24.05.2023' , 'ACTIVE');
